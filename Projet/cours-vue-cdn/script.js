682fe9690ea2df71c1d8e01170ee5b594e17339f
Nathan HYPPOLITE <ihyppolite971@gmail.com>
2023-03-20 13:43:12 +0100

    balise template pour ne pas charger le dom

--- Projet/cours-vue-cdn/script.js
+++ Projet/cours-vue-cdn/script.js
@@ -5,13 +5,23 @@ const app=Vue.createApp({
         return {
             message : "<span>Hello world</span>",
             tab : [2,3,8,5],
-            personne : { id : 1 , nom : 'doe', prenom : 'toto'}
+            personne : { id : 1 , nom : 'doe', prenom : 'john'},
+            nom : "wick",
+            personnes: [
+                { id: 100, nom: 'Wick', prenom: 'John' },
+                { id: 101, nom: 'Abruzzi', prenom: 'John' },
+                { id: 102, nom: 'Marley', prenom: 'Bob' },
+                { id: 103, nom: 'Segal', prenom: 'Steven' }
+            ]
         }
     },
 
     methods : {
             direBonjour(){
-                return 'Bienvenue dans le cours Vue.js 3'
+                return `Bienvenue ${this.nom} dans le cours Vue.js 3 `
+            },
+            bonjourNom(nom){
+                return `Bienvenue ${nom}  `
             }
     }
 })
