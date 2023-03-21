af6db60a0601f677600025eb707552556e39f20d
Nathan HYPPOLITE <ihyppolite971@gmail.com>
2023-03-21 14:26:21 +0100

    correction formateur computed

--- Projet/cours-vue-cli/src/components/ReactiveValue.vue
+++ Projet/cours-vue-cli/src/components/ReactiveValue.vue
@@ -1,5 +1,5 @@
 <script>
-import { computed,  } from 'vue';
+
 
 export default {
   name: "ReactiveValue",
@@ -7,19 +7,22 @@ export default {
     return {
       value1: 0,
       value2: 0,
-      resultat: 0,
+      //resultat: 0,
     };
   },
   created() {
         // watchEffect(() => {
         //     this.value3 = this.value1 + this.value2;
         // })
-        this.resultat = computed(()=>this.value1 + this.value2)
+        // this.resultat = computed(()=>this.value1 + this.value2)
     
   },
   mounted() {
     setInterval(() => (this.value1 += 1), 2000);
   },
+  computed : {
+    resultat (){  return this.value1 + this.value2 }
+  }
 };
 </script>
 
