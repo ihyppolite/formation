a4f2ba30445fd33d09cec3cea6f283805fda09e5
Nathan HYPPOLITE <ihyppolite971@gmail.com>
2023-03-22 10:08:32 +0100

    formulaire avec vee

--- Projet/cours-vue-cli/src/components/PersonneAdd.vue
+++ Projet/cours-vue-cli/src/components/PersonneAdd.vue
@@ -1,24 +1,25 @@
 <template lang="ajouterPersonne">
   <h2></h2>
-  <form  @submit.prevent="ajouterPersonne "> <!-- le prevent permet de ne pas recharger la page -->
+  <Form  @submit.prevent="ajouterPersonne "> <!-- le prevent permet de ne pas recharger la page -->
     <div>
-      Nom  : <input type="text" v-model="personne.nom" />
+      Nom  : <Field type="text" v-model="personne.nom" />
     </div>
   
   <div>
-      Prenom : <input type="text" v-model="personne.prenom" />
+      Prenom : <Field type="text" v-model="personne.prenom" />
     </div>
     <div>
-       Age : <input type="text"  v-model="personne.age" />
+       Age : <Field type="text"  v-model="personne.age" />
     </div>
     <button>
       ajouterPersonne
     </button>
-  </form>
+  </Form>
   
 </template>
 
 <script>
+import { Field , Form} from 'vee-validate'
 export default {
     data(){
         return {
@@ -32,6 +33,10 @@ export default {
         ajouterPersonne(){
             console.log(this.personne)
         }
+    },
+    components :{
+      Field,
+      Form
     }
 };
 </script>
