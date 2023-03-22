a2927a3494f7fb231c65de37c4f305b26c636b20
Nathan HYPPOLITE <ihyppolite971@gmail.com>
2023-03-22 11:24:57 +0100

    ajout des yup champ apr champ model

--- Projet/cours-vue-cli/src/components/PersonneAdd.vue
+++ Projet/cours-vue-cli/src/components/PersonneAdd.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>Ajouter une Personne</h2>
-  <Form @submit="ajouterPersonne">
+  <Form @submit="ajouterPersonne" >
       <div>
           Nom :
           <Field name="nom" type="text" :rules="validateName"/>
@@ -13,7 +13,8 @@
       </div>
       <div>
           Age :
-          <Field name="age" type="number" :rules="'minMax:10,15'"/> <!--utilisation de fonction globale de vee-valide-->
+          <!-- <Field name="age" type="number" :rules="'minMax:10,150'"/> utilisation de fonction globale de vee-valide -->
+          <Field name="age" type="number" :rules="validateage" />
           <ErrorMessage name="age"/>
       </div>
       <button>
@@ -25,7 +26,7 @@
 <script>
 // npm i vee-validate
 import { Field, Form , ErrorMessage } from 'vee-validate'
-
+import  * as yup from 'yup'
 export default {
   components: {
       Field,
@@ -34,22 +35,32 @@ export default {
   },
   data() {
       return {
-          personne: { nom: null, prenom: null, age: null }
+          // validateur avec yup 
+          validateName : yup
+          .string()
+          .required("Le champs est obligatoire ")
+          .matches(/^[A-Z]{1}[a-z]{19}$/ , "Le champs doit commencer par une Maj est contenir mon de 20 caracteres "),
+          validateage : yup
+          .number()
+          .required(" le champs est obligatoire ")
+          .min(0, "age min 0")
+          .max(150, " age max 150")
+
       }
   },
   methods: {
       ajouterPersonne(values) {
           console.log(values)
       },
-      validateName(value) {
-          if (!value) {
-              return "Le champ est obligatoire"
-          }
-          if (value[0] < 'A' || value[0] > 'Z') {
-              return "La première lettre doit être en majuscule"
-          }
-          return true
-      },
+      // validateName(value) {
+      //     if (!value) {
+      //         return "Le champ est obligatoire"
+      //     }
+      //     if (value[0] < 'A' || value[0] > 'Z') {
+      //         return "La première lettre doit être en majuscule"
+      //     }
+      //     return true
+      // },
       // validateAge(value) {
       //     if (!value) {
       //         return "Le champ est obligatoire"
--- Projet/cours-vue-cli/src/validators/min-max.validator.js
+++ Projet/cours-vue-cli/src/validators/min-max.validator.js
@@ -2,15 +2,15 @@ import { defineRule } from "vee-validate";
 
 // fichiers pour fonctionn globale de vee-validate 
 
-defineRule('minMax', (value,[ min , max]) => {
-
+defineRule('minMax', (value,[ min = 0 , max = 55]) => {
+    const valeur = Number(value)
     if (!value) {
         return "Le champ est obligatoire"
     }
-    if (value < min) {
+    if (valeur < min) {
         return `La valeur doit être supérieure ou égale à ${min}`
     }
-    if (value > max) {
+    if (valeur > max) {
         return `La valeur doit être inférieure ou égale à ${max}`
     } 
     return true
