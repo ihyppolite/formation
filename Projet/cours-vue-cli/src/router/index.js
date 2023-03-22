a1753556ac0bd042dc40a78c0bde09cb9ae0597f
Nathan HYPPOLITE <ihyppolite971@gmail.com>
2023-03-22 09:16:54 +0100

    chemin en erreur et redirection

--- Projet/cours-vue-cli/src/router/index.js
+++ Projet/cours-vue-cli/src/router/index.js
@@ -1,64 +1,70 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import PersonneDetailsView from '../views/PersonneDetailsView.vue'
-import PersonneShowView from '../views/PersonneShowView.vue'
-import AdresseView from '@/views/AdresseView.vue'
-import TableauView from '../views/TableauView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PersonneDetailsView from "../views/PersonneDetailsView.vue";
+import PersonneShowView from "../views/PersonneShowView.vue";
+import AdresseView from "@/views/AdresseView.vue";
+import TableauView from "../views/TableauView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes = [
   //alias
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
-    alias: ['/home', '/accueil']
+    alias: ["/home", "/accueil"],
   },
   {
-    path: '/adresse',
-    name: 'adresse',
-    component: AdresseView
+    path: "/adresse",
+    name: "adresse",
+    component: AdresseView,
   },
   {
-    path: '/personne',
-    props :true ,
-    name: 'personne-show',
-    component: PersonneShowView
-  },
-  {
-    path: '/tableau/:id',
+    path: "/personne",
     props: true,
-    name: 'tableau',
-    component: TableauView
+    name: "personne-show",
+    component: PersonneShowView,
   },
   {
-    path: '/personne/:id',
-    props :true ,
-    name: 'personne-details',
-    component: PersonneDetailsView
+    path: "/tableau/:id",
+    props: true,
+    name: "tableau",
+    component: TableauView,
   },
- 
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
+    path: "/personne/:id",
+    props: true,
+    name: "personne-details",
+    component: PersonneDetailsView,
   },
-  // redirection 
-  {
-    path: '/person',
-    redirect: '/personne'
 
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("../views/AboutView.vue"),
+  },
+  // redirection
+  {
+    path: "/person",
+    redirect: "/personne",
   },
   {
-    path: '/adresse/:rue/:codePostal/:ville',
+    path: "/adresse/:rue/:codePostal/:ville",
     redirect: (chemin) => {
-      return { path: '/adresse', query: { ...chemin.params } }
-    }
+      return { path: "/adresse", query: { ...chemin.params } };
+    },
   },
-]
+  {
+    // path:'/:catchALL(.*)', pour redirection pour les liens sans correspondant  avec VUE2
+    path: "/:pathMatch(.*)*", //le * a la fin permet de decomposer le lien 
+    name: "error",
+    component: NotFoundView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ Projet/cours-vue-cli/src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        404 : La route '{{ $route.params.pathMatch }}' non trouvée
+    </div>
+</template>
+
+<style scoped>
+div {
+    color: red
+}
+</style>
